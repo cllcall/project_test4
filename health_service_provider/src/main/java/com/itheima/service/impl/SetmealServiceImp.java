f4a15bd8f2a1954c65ac0f9f12ee5c54acb06556
cllcall <2236445873@qq.com>
2021-03-28 01:49:52 +0800

    台式20210327晚。

--- health_service_provider/src/main/java/com/itheima/service/impl/SetmealServiceImp.java
+++ health_service_provider/src/main/java/com/itheima/service/impl/SetmealServiceImp.java
@@ -10,10 +10,17 @@ import com.itheima.entity.QueryPageBean;
 import com.itheima.pojo.CheckGroup;
 import com.itheima.pojo.Setmeal;
 import com.itheima.service.SetmealService;
+import freemarker.template.Configuration;
+import freemarker.template.Template;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
 import redis.clients.jedis.JedisPool;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.Writer;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -28,6 +35,11 @@ public class SetmealServiceImp implements SetmealService{
     private SetmealDao setmealDao;
     @Autowired
     private JedisPool jedisPool;
+    @Autowired
+    private FreeMarkerConfigurer freeMarkerConfigurer;
+    @Value("${out_put_path}")
+    private String outPutPath;//从属性文件中读取要生成的html对应的目录
+
     //新增套餐信息，同时关联检查组
     public void add(Setmeal setmeal, Integer[] checkgroupIds) {
         setmealDao.add(setmeal);
@@ -36,6 +48,53 @@ public class SetmealServiceImp implements SetmealService{
         //将图片名称保存到Radis集合中
         String fileName = setmeal.getImg();
         jedisPool.getResource().sadd(RedisConstant.SETMEAL_PIC_DB_RESOURCES,fileName);
+
+        //当添加套餐后需要重新生成静态页面（套餐列表页面、套餐详情页面）
+        generateMobileStaticHtml();
+    }
+
+    //生成当前方法所需的静态页面
+    public void generateMobileStaticHtml(){
+        //在生成静态页面之前需要查询数据
+        List<Setmeal> list = setmealDao.findAll();
+
+        //需要生成套餐列表静态页面
+        generateMobileSetmealListHtml(list);
+
+        //需要生成套餐详情静态页面
+        generateMobileSetmealDetailHtml(list);
+    }
+
+    //生成套餐列表静态页面
+    public void generateMobileSetmealListHtml(List<Setmeal> list){
+        Map map = new HashMap();
+        map.put("setmealList",list);
+        generteHtml("mobile_setmeal.ftl","m_setmeal.html",map);
+    }
+
+    //生成套餐详情静态页面（可能有多个）
+    public void generateMobileSetmealDetailHtml(List<Setmeal> list){
+        for(Setmeal setmeal : list){
+            Map map = new HashMap();
+            map.put("setmeal",setmealDao.findById(setmeal.getId()));
+            generteHtml("mobile_setmeal_detail.ftl","setmeal_detail_"+setmeal.getId()+".html",map);
+        }
+    }
+
+    //通用方法，用于生成静态页面
+    public void generteHtml(String templateName,String htmlPageName,Map map){
+        Configuration configuration = freeMarkerConfigurer.getConfiguration();
+        Writer out = null;
+        try {
+            Template template = configuration.getTemplate(templateName);
+            //构造输出流
+            out = new FileWriter(new File(outPutPath+"/"+htmlPageName));
+            //输出文件
+            template.process(map,out);
+            out.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     //分页查询
